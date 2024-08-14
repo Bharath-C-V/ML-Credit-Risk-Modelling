143800a91635dd5d056ca8ad94dafca4ca34b4e0
Bharath C V <bharathcv26@gmail.com>
2024-08-14 22:35:52 +0530

    changes

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ row4 = st.columns(3)
 
 # Assign inputs to the first row with default values
 with row1[0]:
-    age = st.number_input('Age', min_value=18, step=1, max_value=100, value=28)
+    age = st.number_input('Age', min_value=18, step=1, max_value=100, value=18)
 with row1[1]:
-    income = st.number_input('Income', min_value=0, value=1200000)
+    income = st.number_input('Income', min_value=0, value=1000000)
 with row1[2]:
-    loan_amount = st.number_input('Loan Amount', min_value=0, value=2560000)
+    loan_amount = st.number_input('Loan Amount', min_value=0, value=500000)
 
 # Calculate Loan to Income Ratio and display it
 loan_to_income_ratio = loan_amount / income if income > 0 else 0
@@ -42,7 +42,7 @@ with row2[0]:
 
 # Assign inputs to the remaining controls
 with row2[1]:
-    loan_tenure_months = st.number_input('Loan Tenure (months)', min_value=0, step=1, value=36)
+    loan_tenure_months = st.number_input('Loan Tenure (months)', min_value=0, step=1, value=48)
 with row2[2]:
     avg_dpd_per_delinquency = st.number_input('Avg DPD', min_value=0, value=20)
 
@@ -51,7 +51,7 @@ with row3[0]:
 with row3[1]:
     credit_utilization_ratio = st.number_input('Credit Utilization Ratio', min_value=0, max_value=100, step=1, value=30)
 with row3[2]:
-    num_open_accounts = st.number_input('Open Loan Accounts', min_value=1, max_value=4, step=1, value=2)
+    num_open_accounts = st.number_input('Open Loan Accounts', min_value=1, max_value=4, step=1, value=1)
 
 
 with row4[0]:
